192300df8e3f543af7495d83a1df999192d34c21
Anas Misbahuddin <anas.misbahuddin4869@gmail.com>
2021-03-24 21:24:14 +0800

    splitting code into func

--- basic/assets/scripts/app.js
+++ basic/assets/scripts/app.js
@@ -1,11 +1,18 @@
 const defaultResult = 0
 let currentResult = defaultResult
 
+function getUserNumberInput() {
+    return parseInt(userInput.value)
+}
+
 function add(){
-    currentResult = currentResult + parseInt(userInput.value);
+    const enteredNumber = getUserNumberInput()
+    const calcDescription = `${currentResult} + ${enteredNumber}`
+    currentResult = currentResult + enteredNumber;
     // currentResult = currentResult.toString() + parseInt(userInput.value);
     // currentResult = currentResult + +userInput.value;
-    outputResult(currentResult,'')
+
+    outputResult(currentResult,calcDescription)
 }
 
 addBtn.addEventListener('click',add)
