182727a9523bc0d333c984fd2fb32d8548ca69bc
Anas Misbahuddin <anas.misbahuddin4869@gmail.com>
2021-03-24 22:23:53 +0800

    using array

--- basic/assets/scripts/app.js
+++ basic/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0
 let currentResult = defaultResult
+let logEntries = []
 
 function getUserNumberInput() {
     return parseInt(userInput.value)
@@ -16,6 +17,8 @@ function add(){
     currentResult += enteredNumber;
     // currentResult++ increment
     createAndWriteOutput('+',initialResult,enteredNumber)
+    logEntries.push(enteredNumber)
+    console.log(logEntries[1]);
 }
 
 function subtract() {
